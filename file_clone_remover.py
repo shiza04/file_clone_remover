a00318de25098347182c8274217442202cbb5a97
shiza04 <143140529+shiza04@users.noreply.github.com>
2023-09-01 20:21:59 +0200

    Update file_clone_remover.py

--- file_clone_remover.py
+++ file_clone_remover.py
@@ -10,10 +10,9 @@ for el in dirs:
     for file in os.listdir():
         if not os.path.isdir(file):
             data = open(file, "rb").read()
-            print(hash(data))
             if hash(data) in saved_elements:
                 os.system(f"del \"{file}\"")
+                print(f"Deleted {file} at position {el}")
             else:
                 saved_elements[hash(data)] = file
-    print(el, saved_elements)
     os.chdir("..")
