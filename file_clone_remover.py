b7a8c1a98c8a4bf664ecb22f319ac659b278fbaa
shiza04 <143140529+shiza04@users.noreply.github.com>
2023-09-01 19:21:43 +0200

    Add files via upload

---
+++ file_clone_remover.py
@@ -0,0 +1,20 @@
+import os
+
+dirs = [x for x in os.listdir() if os.path.isdir(x)] + ["."]
+
+print(dirs)
+
+for el in dirs:
+    os.chdir(el)
+    saved_elements = {}
+    for file in os.listdir():
+        if not os.path.isdir(file):
+            data = open(file, "rb").read()
+            print(hash(data))
+            if hash(data) in saved_elements:
+                print(f"{el} already exists")
+                #os.system(f"del \"{file}\"")
+            else:
+                saved_elements[hash(data)] = file
+    print(el, saved_elements)
+    os.chdir("..")
